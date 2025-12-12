9f926e2912c12460238919338365bec2c030d3a9
genekellyboyle <DrDuffy@bhhsnv.com>
2025-12-11 20:22:38 -0800

    Add Google Analytics tracking code (G-MPLYNTN4V7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,12 +130,17 @@ export default function RootLayout({
           src='https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID'
           strategy='afterInteractive'
         />
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=G-MPLYNTN4V7'
+          strategy='afterInteractive'
+        />
         <Script id='google-analytics' strategy='afterInteractive'>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'GA_MEASUREMENT_ID');
+            gtag('config', 'G-MPLYNTN4V7');
           `}
         </Script>
 
